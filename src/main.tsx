e9c2ca9f4610758f0e3c1f87e804d54dd89cb055
figuierebaptistepro-sys <figuierebaptistepro@gmail.com>
2025-10-01 16:28:57 +0200

    Check and fix AppKit imports to avoid duplicates

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Import AppKit configuration
+import './config/reown'
+
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
